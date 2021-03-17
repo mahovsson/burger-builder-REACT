c858671f4eb2362afd41d411109053c8274647ee
mahovsson <matus137@gmail.com>
2021-03-17 15:04:34 +0100

    Order fetching from database

--- src/components/Navigation/Navigationitems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/Navigationitems/NavigationItem/NavigationItem.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import classes from './NavigationItem.css'
+import classes from './NavigationItem.css';
+import { NavLink } from 'react-router-dom';
 
 const naviagationItem = (props) => {
   return (
     <li className={classes.NavigationItem}>
-    <a
-      href={props.link}
-      className={props.active ? classes.active : null}
-      >
-      {props.children}</a>
+      <NavLink
+        exact
+        to={props.link}
+        activeClassName={classes.active}
+        >{props.children}</NavLink>
+
     </li>
    );
 }
