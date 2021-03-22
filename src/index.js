e6038356d6bdc24da7e03916b3d4e8ac40670f5c
mahovsson <matus137@gmail.com>
2021-03-22 09:29:42 +0100

    redux added to the index js file and reducer added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom';
+import reducer from './store/reducer'
+
+const store = createStore(reducer);
 
 const app = (
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+      <Router>
+          <App />
+      </Router>
+  </Provider>
+
 )
 
 ReactDOM.render( app, document.getElementById('root'));
