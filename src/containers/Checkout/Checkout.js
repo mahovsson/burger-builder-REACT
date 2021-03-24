a6601a713f43d5d0d087f68c60c4cf546c4e3022
mahovsson <matus137@gmail.com>
2021-03-24 09:42:57 +0100

    checkout fixed

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary'
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 import ContactData from './ContactData/ContactData'
 import { connect } from 'react-redux'
 
@@ -16,20 +16,24 @@ class Checkout extends Component {
 
 
   render() {
-    return (
-      <div>
-        <CheckoutSummary
-          ingredients={this.props.ings}
-          checkoutCancel={this.checkoutCancelHandler}
-          checkoutContinue={this.checkoutContinueHandler}
-        />
-        <Route
-          path={this.props.match.path + '/contact-data'}
-          component={ContactData}
-        />
-      </div>
+    let summary = <Redirect to="/" />
+      if (this.props.ings) {
+        summary = (
+          <div>
+              <CheckoutSummary
+              ingredients={this.props.ings}
+              checkoutCancel={this.checkoutCancelHandler}
+              checkoutContinue={this.checkoutContinueHandler}
+            />
+            <Route
+              path={this.props.match.path + '/contact-data'}
+              component={ContactData}
+            />
+        </div>
+        )
+      }
 
-     );
+    return summary;
   }
 }
 
