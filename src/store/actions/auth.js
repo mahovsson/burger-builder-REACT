aead9c8dc2c6f1c49c11cae0eb569c2e5d375a0b
mahovsson <matus137@gmail.com>
2021-03-29 17:54:48 +0200

    logout token and function

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,20 @@ export const authFail = (error) => {
   };
 };
 
+export const logout = () => {
+  return {
+    type:actionTypes.AUTH_LOGOUT
+  };
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout())
+    }, expirationTime * 1000);
+  }
+}
+
 export const auth = (email, password, isSignup) => {
   return dispatch => {
       dispatch(authStart());
@@ -40,10 +54,11 @@ export const auth = (email, password, isSignup) => {
         .then(res => {
           console.log(res);
           dispatch(authSuccess(res.data.idToken, res.data.localId));
+          dispatch(checkAuthTimeout(res.data.expiresIn));
         })
         .catch(err => {
           console.log(err);
-          dispatch(authFail(err.response));
+          dispatch(authFail(err.response.data.error));
         }
         )
   }
