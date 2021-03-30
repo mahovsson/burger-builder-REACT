9c0ae9b072e6b5734c45d0ab4387ca7926043d56
mahovsson <matus137@gmail.com>
2021-03-30 13:57:52 +0200

    fixed bugs and console logs

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,18 +4,16 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-        console.log(props.ingredients);
-        console.log( Object.keys(props.ingredients));
         let transformedIngredients = Object.keys(props.ingredients)// this gives [] of ["salad", "cheese", "tikki", "tanduri"]
                 .map(igKey => { // <<= since we want to execute a function on each item of [] i.e ["salad", "cheese", "tikki", "tanduri"]
-                  console.log(props.ingredients[igKey]); // gives quantity of each [] element ...salad, cheese...etc.... How? Key, Value Pair...you provide key [igKey] and it gives value ...for ex: cheese: 4, where cheese is key and 4 is value
+                   // gives quantity of each [] element ...salad, cheese...etc.... How? Key, Value Pair...you provide key [igKey] and it gives value ...for ex: cheese: 4, where cheese is key and 4 is value
                   // creating empty arrays of size = qualtity of each ingredient..for ex if you say
                   // ...Array(2) it creates two elements in array [undefined] and [undefined]
                   return [...Array(props.ingredients[igKey])]
                      //now we are going to fill up this empty array elements = qty of each ingredient,
                      // with real ingredient jsx code
                      .map((_,i) => {
-                          console.log(igKey + i); //to give unique name to each key....chicken01,chicken 02 etc...
+                           //to give unique name to each key....chicken01,chicken 02 etc...
                           return <BurgerIngredient key={igKey + i} type={igKey} />;// key because we are returning
                           // an array of jsx elements and any Array requires keys
                           // ....means we are mapping/processing our object in to an array of ingredients
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -48,7 +48,7 @@ class BurgerIngredient extends Component {
     }
 }
 
-BurgerIngredient.PropTypes = {
+BurgerIngredient.propTypes = {
   type: PropTypes.string.isRequired
 };
 
