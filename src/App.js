5f7b0aca767ff3c68c3592552809f787e327cde8
mahovsson <matus137@gmail.com>
2021-04-07 13:45:13 +0200

    Lazy loading adjusted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React, { useEffect } from 'react';
-import asyncComponent from './hoc/asyncComponent/asyncComponent';
-
+import React, { useEffect, Suspense} from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import {Route, Switch, withRouter, Redirect} from 'react-router-dom';
@@ -8,15 +6,15 @@ import Logout from './containers/Auth/Logout/Logout'
 import { connect } from 'react-redux';
 import * as actions from './store/actions/index';
 
-const asyncCheckout = asyncComponent(() => {
+const Checkout = React.lazy(() => {
   return import('./containers/Checkout/Checkout');
 });
 
-const asyncOrders = asyncComponent(() => {
+const Orders = React.lazy(() => {
   return import('./containers/Orders/Orders');
 });
 
-const asyncAuth = asyncComponent(() => {
+const Auth = React.lazy(() => {
   return import('./containers/Auth/Auth');
 });
 
@@ -27,7 +25,7 @@ const app = props => {
 
     let routes = (
         <Switch>
-           <Route path="/auth" component={asyncAuth} />
+           <Route path="/auth" render={() => <Auth />} />
            <Route path="/" exact component={BurgerBuilder} />
            <Redirect to="/" />
         </Switch>
@@ -36,10 +34,10 @@ const app = props => {
     if (props.isAuthenticated) {
         routes = (
           <Switch>
-           <Route path="/checkout" component={asyncCheckout} />
-           <Route path="/orders" component={asyncOrders} />
+           <Route path="/checkout" render={() => <Checkout />} />
+           <Route path="/orders" render={() => <Orders />} />
            <Route path="/logout" component={Logout} />
-           <Route path="/auth" component={asyncAuth} />
+           <Route render={() => <Auth />} />
            <Route path="/" exact component={BurgerBuilder} />
            <Redirect to="/" />
         </Switch>
@@ -49,7 +47,7 @@ const app = props => {
     return (
      <div>
        <Layout>
-         {routes}
+         <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
        </Layout>
      </div>
     );
