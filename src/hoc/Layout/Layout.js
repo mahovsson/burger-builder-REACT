6ad906d6178143c3109c1e4026522f77391273a5
mahovsson <matus137@gmail.com>
2021-04-07 13:53:05 +0200

    layout re-written to functional component

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,42 +1,37 @@
-import React,{ Component } from 'react';
+import React,{ useState } from 'react';
 import { connect } from 'react-redux';
 import Aux from '../Aux/Aux';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
-class Layout extends Component {
-  state = {
-    showSideDrawer: false
+const layout = props => {
+
+  const [ sideDrawerIsVisible, setsideDrawerIsVisible ] = useState(false);
+
+  const sideDrawerClosedHandler = () => {
+    setsideDrawerIsVisible(false);
   }
 
-  sideDrawerClosedHandler = () => {
-    this.setState({showSideDrawer:false});
+  const sideDrawerToggleHandler = () => {
+    setsideDrawerIsVisible(!sideDrawerIsVisible);
   }
 
-  sideDrawerToggleHandler = () => {
-    this.setState((prevState) => {
-      return { showSideDrawer: !prevState.showSideDrawer }
-    })
-  }
-
-  render () {
     return(
       <Aux>
         <Toolbar
-          isAuth={this.props.isAuthenticated}
-          drawerToggleClicked={this.sideDrawerToggleHandler}/>
+          isAuth={props.isAuthenticated}
+          drawerToggleClicked={sideDrawerToggleHandler}/>
         <SideDrawer
-          isAuth={this.props.isAuthenticated}
-          open={this.state.showSideDrawer}
-          closed={this.sideDrawerClosedHandler}/>
+          isAuth={props.isAuthenticated}
+          open={sideDrawerIsVisible}
+          closed={sideDrawerClosedHandler}/>
         <main className={classes.Content}>
-          {this.props.children}
+          {props.children}
         </main>
       </Aux>
     )
   }
-}
 
 const mapStateToProps = state => {
   return {
@@ -44,4 +39,4 @@ const mapStateToProps = state => {
   };
 }
 
-export default connect(mapStateToProps)(Layout);
+export default connect(mapStateToProps)(layout);
